b11faa4b65ce7c090c323bec22e048da39b3be59
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-07-05 14:16:30 +0100

    Add project files.

---
+++ Files/FileProcessor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Files
+{
+    internal class FileProcessor
+    {
+        private const string BackUpDirectoryName = "backup";
+        private const string InProgressDirectoryName = "processing";
+        private const string CompletedDirectoryName = "complete";
+
+        public string InputFilePath { get; }
+        public FileProcessor(string filePath) => InputFilePath = filePath;
+
+        public void Process()
+        {
+            WriteLine($"Begin process of {InputFilePath}");
+
+            // Check if file exist
+            if (!File.Exists(InputFilePath))
+            {
+                WriteLine($"ERROR: file {InputFilePath} does not exist.");
+                return;
+            }
+
+            // Parent path name
+            string rootDirectoryPath = new DirectoryInfo(InputFilePath).Parent.FullName;
+            if (rootDirectoryPath == null )
+            {
+                throw new InvalidOperationException($"Cannot determine root directory path");
+            }
+            WriteLine($"Root data path is {rootDirectoryPath}");
+
+            // string rootsDirectoryPath = new DirectoryInfo(InputFilePath).Parent.Parent.FullName;
+            // WriteLine($"Next root data path is {rootsDirectoryPath}");
+
+            // Check if backup dir exists
+            string backupDirectoryPath = Path.Combine(rootDirectoryPath, BackUpDirectoryName);
+
+            if (!Directory.Exists(backupDirectoryPath))
+            {
+                WriteLine($"Creating: {backupDirectoryPath}");
+                Directory.CreateDirectory(backupDirectoryPath);
+            }
+
+            // Copy file to backup dir
+            string inputFileName = Path.GetFileName(InputFilePath);
+            string backupFilePath = Path.Combine(backupDirectoryPath, inputFileName);
+            WriteLine($"Copying {InputFilePath} to {backupFilePath}");
+            File.Copy(InputFilePath, backupFilePath, true);
+
+            // Move to in progress dir
+            Directory.CreateDirectory(Path.Combine(rootDirectoryPath, InProgressDirectoryName));
+            string inProgressFilePath = Path.Combine(rootDirectoryPath, InProgressDirectoryName, inputFileName);
+
+            if (File.Exists(inProgressFilePath))
+            {
+                WriteLine($"ERROR: file {inProgressFilePath} already exist"); //delete file already within to not get this error
+                return;
+            }
+
+            WriteLine($"Moving {inputFileName} to {inProgressFilePath}");
+            File.Move(InputFilePath, inProgressFilePath);
+        }
+    }
+}
---
+++ Files/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Files;
+using static System.Console;
+
+namespace Files
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            WriteLine("Parsing command line options");
+            var command = args[0];
+            if (command == "--file")
+            {
+                var filePath = args[1];
+
+                // Check if path is absolute
+                if (!(Path.IsPathRooted(filePath) && !filePath.StartsWith(@"\\")))
+                {
+                    WriteLine($"ERROR: path '{filePath}' must be fully qualified");
+                    ReadLine();
+                    return;
+
+                }
+
+                WriteLine($"Single file {filePath} selected");
+                ProcessSingleFile(filePath);
+            }
+            else if (command == "--dir")
+            {
+                var directoryPath = args[1];
+                var fileType = args[2];
+                WriteLine($"Directory {directoryPath} selected for {fileType} files");
+                ProcessDirectory(directoryPath, fileType);
+            }
+            else
+            {
+                WriteLine("invalid");
+            }
+
+            ReadLine();
+
+            WriteLine("------------------------");
+            string path = @"C:\programs\file.txt";
+            // Get file name.
+            string filename = Path.GetFileName(path);
+
+            WriteLine("PATH:     {0}", path);
+            WriteLine("FILENAME: {0}", filename);
+            string filename2 = Path.GetFileNameWithoutExtension(path);
+
+            WriteLine("PATH:         {0}", path);
+            WriteLine("NO EXTENSION: {0}", filename2);
+            string value1 = @"C:\perls\word.txt";
+            string value2 = @"C:\file.excel.dots.xlsx";
+
+            // ... Get extensions.
+            string ext1 = Path.GetExtension(value1);
+            string ext2 = Path.GetExtension(value2);
+            WriteLine(ext1);
+            WriteLine(ext2);
+
+            WriteLine("------------------------");
+
+            // Put all file names in root directory into array.
+            string[] array1 = Directory.GetFiles(@"C:\Users\tayyi\Downloads\");
+
+            // Put all bin files in root directory into array.
+            // ... This is case-insensitive.
+            string[] array2 = Directory.GetFiles(@"C:\", "*.BIN");
+
+            // Display all files.
+            WriteLine("--- Files: ---");
+            foreach (string name in array1)
+            {
+                WriteLine(name);
+            }
+
+            // Display all BIN files.
+            WriteLine("--- BIN Files: ---");
+            foreach (string name in array2)
+            {
+                WriteLine(name);
+            }
+
+            WriteLine("------------------------");
+            ReadLine();
+        }
+
+        static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+
+        static void ProcessDirectory(string DirectoryPath, string fileType)
+        {
+            
+        }
+    }
+}
